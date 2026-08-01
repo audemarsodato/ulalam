99480d3e7536f07a459f6c91653d38e4c7175f4e
Audemars Odato <audemarsodato@gmail.com>
2026-08-01 21:49:29 +0800

    feat: Add scroll to top on page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import ScrollToTop from './components/utils/ScrollToTop'
+
 import Home from './pages/Home'
 import CreateUlam from './pages/CreateUlam'
 import EditUlam from './pages/EditUlam'
@@ -7,12 +9,15 @@ import CreateVariation from './pages/CreateVariation'
 import Search from './pages/Search'
 import UlamProfile from './pages/UlamProfile'
 import CookUlam from './pages/cook-ulam/CookUlam'
+import CookingHistory from './pages/cooking-history/CookingHistory'
 
 export default function App() {
 
         return (
                 <div className='app'>
                         <BrowserRouter>
+                                <ScrollToTop />
+
                                 <Routes>
                                         <Route path='/' element={<Home />}/>
 
@@ -26,6 +31,7 @@ export default function App() {
                                         <Route path='/search' element={<Search />}/>
 
                                         <Route path='/cook/:ulamId' element={<CookUlam />}/>
+                                        <Route path='/cook/history' element={<CookingHistory />}/>
                                 </Routes>              
                         </BrowserRouter>
                 </div>
