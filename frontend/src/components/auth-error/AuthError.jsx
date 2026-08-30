1cfedf8fab8c7e3c5cfbf6cad20b8fdb3ebc97ff
Audemars Odato <audemarsodato@gmail.com>
2026-08-30 22:28:51 +0800

    feat: Create login/signup error card

---
+++ frontend/src/components/auth-error/AuthError.jsx
@@ -0,0 +1,11 @@
+import './AuthError.css'
+
+export default function AuthError({ message }) {
+
+        return (
+                <section className="auth-error-card">
+                        <span className="material-symbols-rounded">cancel</span>
+                        <p className="error-card__message">{message}</p>
+                </section>
+        )
+}
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Signup.css'
 import googleIcon from '../../assets/icons/google-icon.svg'
+import AuthError from '../../components/auth-error/AuthError'
 
 export default function Signup() {
+        const [ email, setEmail ] = useState('')
+        const [ password, setPassword ] = useState('')
+        const [ confirmPassword, setConfirmPassword ] = useState('')
+        const [ error, setError ] = useState(null)
+
+        const handleSubmit = (event) => {
+                event.preventDefault()
+
+                if (password.trim() !== confirmPassword.trim()) {
+                        setError({message: 'Password does not match'})
+                        return
+                }
+
+                // signup fetch
+        }
 
         return (
                 <section className="signup-page">
@@ -12,23 +29,48 @@ export default function Signup() {
                                 <h2 className="signup__h2">Create Account</h2>
                         </header>
 
-                        {/* Error message component */}
+                        {error && 
+                                <AuthError message={error.message}/>
+                        }
 
                         <form className="signup__form">
                                 <div className="signup__form__email-input">
                                         <label for='email' className="signup__email label">Email</label>
-                                        <input type="email" name="email" id="email" className="signup__email input" />
-                                </div>
-                                <div className="signup__form__password-input">
-                                        <label for='password' className="signup__password label">Password</label>
-                                        <input type="password" name="password" id="password" className="signup__password input" />
-                                </div>
-                                <div className="signup__form__confirm-password-input">
-                                        <label for='confirm-password' className="signup__confirm-password label">Confirm Password</label>
-                                        <input type="password" name="confirm-password" id="confirm-password" className="signup__confirm-password input" />
+                                        <input 
+                                                type="email" 
+                                                name="email" 
+                                                id="email" 
+                                                className="signup__email input" 
+                                                onChange={event => setEmail(event.target.value)}
+                                                value={email}
+                                        />
                                 </div>
 
-                                <button type="submit" className='signup__signup-button'>Sign Up</button>
+                                <div className="signup__form__password-input">
+                                        <label for='password' className="signup__password label">Password</label>
+                                        <input 
+                                                type="password" 
+                                                name="password" 
+                                                id="password" 
+                                                className="signup__password input" 
+                                                onChange={event => setPassword(event.target.value)}
+                                                value={password}
+                                        />
+                                </div>
+
+                                <div className="signup__form__confirm-password-input">
+                                        <label for='confirm-password' className="signup__confirm-password label">Confirm Password</label>
+                                        <input 
+                                                type="password" 
+                                                name="confirm-password" 
+                                                id="confirm-password" 
+                                                className="signup__confirm-password input" 
+                                                onChange={event => setConfirmPassword(event.target.value)}
+                                                value={confirmPassword}
+                                        />
+                                </div>
+
+                                <button type="submit" onClick={handleSubmit} className='signup__signup-button'>Sign Up</button>
                         </form>
 
                         <section className="signup__actions">
