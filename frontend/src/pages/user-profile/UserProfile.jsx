4b0b18dcf7b6337c2bf2e7135415eaa2af14e3d8
Audemars Odato <audemarsodato@gmail.com>
2026-09-16 21:52:45 +0800

    fix: Add display followers and followings in user profile page

--- frontend/src/pages/user-profile/UserProfile.jsx
+++ frontend/src/pages/user-profile/UserProfile.jsx
@@ -142,6 +142,14 @@ export default function UserProfile() {
                 />
         )
 
+        const displayFollowers = user.followers.map(user => 
+                <UserCard user={user}/>
+        )
+
+        const displayFollowings = user.followings.map(user => 
+                <UserCard user={user}/>
+        )
+
         return (
                 <section className="user-profile-page">
                         <Header pageTitle={'Profile'} />
@@ -252,8 +260,7 @@ export default function UserProfile() {
                         {activeModal === 'followers' &&
                                 <Modal modalTitle={'Followers'} onClose={() => setActiveModal(null)} >
                                         <div className="user-container">
-                                                <UserCard userName={'Audemars Odato'} followerCount={67} followingCount={12} profileURL={'https://scontent.fmnl9-6.fna.fbcdn.net/v/t39.30808-1/636768676_2340190826450286_1542923457919314152_n.jpg?stp=dst-jpg_tt6&cstp=mx206x206&ctp=s200x200&_nc_cat=102&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeGwGF4ONy-jCbTDh10q-PbFZjbUZhg5BwlmNtRmGDkHCeFKmaqHlWsJOsWt1UWRAw0MEVjHyjJZxDqaxaM_vkBz&_nc_ohc=BHooeYjtUdgQ7kNvwG81mro&_nc_oc=Adr_qdkD0jW6H2KyqIq6tdWIkWl5FaYEM_uXyGvXNrE6p8kxhCx5YKoUJSCOUuto2_M&_nc_zt=24&_nc_ht=scontent.fmnl9-6.fna&_nc_gid=bAZ9CknqDwgErVLUKfldeA&_nc_ss=7b2a8&oh=00_AQAY5As_qp_2WM40ExIwnBK7y4uGlny6ibOcmPxG1so2dw&oe=6A6CEA5E'}/>
-                                                <UserCard userName={'Trisha Wyne Bobis'} followerCount={76} followingCount={3} profileURL={'https://scontent.fmnl9-4.fna.fbcdn.net/v/t39.30808-1/615053689_3027601990963411_2061465663798132513_n.jpg?stp=dst-jpg_tt6&cstp=mx1548x1555&ctp=s100x100&_nc_cat=105&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF1CpW5UvpC81o6T7elslKpOvfWlZviofM699aVm-Kh81k7Yg7XKRwOeZPPkeNKjK9k_Iv4BxixlVjEdz32C6rs&_nc_ohc=QfOVOhN1yAMQ7kNvwGOpcfh&_nc_oc=AdqXLdO6h8b4DIXL-hWh-PW2sQLOU4SGvgN_xAAiLo7a172Obe6j8_NnpgBHZjMt2VU&_nc_zt=24&_nc_ht=scontent.fmnl9-4.fna&_nc_gid=XWkCZd4Akf5NaJqzAAhWAA&_nc_ss=7b2a8&oh=00_AQBOWtlAFueUjLnDs0YSlRGnsdhuKo93GOsV1NbCN9OFoA&oe=6A6D030D'}/>
+                                                {displayFollowers}
                                         </div>
                                 </Modal>
                         }
@@ -261,8 +268,7 @@ export default function UserProfile() {
                         {activeModal === 'followings' &&
                                 <Modal modalTitle={'Followings'} onClose={() => setActiveModal(null)} >
                                         <div className="user-container">
-                                                <UserCard userName={'Audemars Odato'} followerCount={67} followingCount={12} profileURL={'https://scontent.fmnl9-6.fna.fbcdn.net/v/t39.30808-1/636768676_2340190826450286_1542923457919314152_n.jpg?stp=dst-jpg_tt6&cstp=mx206x206&ctp=s200x200&_nc_cat=102&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeGwGF4ONy-jCbTDh10q-PbFZjbUZhg5BwlmNtRmGDkHCeFKmaqHlWsJOsWt1UWRAw0MEVjHyjJZxDqaxaM_vkBz&_nc_ohc=BHooeYjtUdgQ7kNvwG81mro&_nc_oc=Adr_qdkD0jW6H2KyqIq6tdWIkWl5FaYEM_uXyGvXNrE6p8kxhCx5YKoUJSCOUuto2_M&_nc_zt=24&_nc_ht=scontent.fmnl9-6.fna&_nc_gid=bAZ9CknqDwgErVLUKfldeA&_nc_ss=7b2a8&oh=00_AQAY5As_qp_2WM40ExIwnBK7y4uGlny6ibOcmPxG1so2dw&oe=6A6CEA5E'}/>
-                                                <UserCard userName={'Trisha Wyne Bobis'} followerCount={76} followingCount={3} profileURL={'https://scontent.fmnl9-4.fna.fbcdn.net/v/t39.30808-1/615053689_3027601990963411_2061465663798132513_n.jpg?stp=dst-jpg_tt6&cstp=mx1548x1555&ctp=s100x100&_nc_cat=105&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF1CpW5UvpC81o6T7elslKpOvfWlZviofM699aVm-Kh81k7Yg7XKRwOeZPPkeNKjK9k_Iv4BxixlVjEdz32C6rs&_nc_ohc=QfOVOhN1yAMQ7kNvwGOpcfh&_nc_oc=AdqXLdO6h8b4DIXL-hWh-PW2sQLOU4SGvgN_xAAiLo7a172Obe6j8_NnpgBHZjMt2VU&_nc_zt=24&_nc_ht=scontent.fmnl9-4.fna&_nc_gid=XWkCZd4Akf5NaJqzAAhWAA&_nc_ss=7b2a8&oh=00_AQBOWtlAFueUjLnDs0YSlRGnsdhuKo93GOsV1NbCN9OFoA&oe=6A6D030D'}/>
+                                                {displayFollowers}
                                         </div>
                                 </Modal>
                         }
